4e33851f7555bf7d882c02696a4a9f785c127f49
msysh <msysh-development@msysh.com>
2023-02-28 14:31:26 +0900

    Initial commit

---
+++ test/codepipeline-deploy-cfn.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodepipelineDeployCfn from '../lib/codepipeline-deploy-cfn-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/codepipeline-deploy-cfn-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodepipelineDeployCfn.CodepipelineDeployCfnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
